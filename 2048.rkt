41b910d90890f14ecbc9e4f034f5432051366c71
ChillingSoundCry <30822585+ChillingSoundCry@users.noreply.github.com>
2018-05-06 00:29:13 +0800

    Update 2048.rkt

--- 2048.rkt
+++ 2048.rkt
@@ -1,3 +1,8 @@
+;(require racket/vector)
+;(define map (vector 1 2 3 4 5 6 7 8))
+;(display (vector-ref map 0))
+;list-tail (return end) 
+;(let ((sx 5) (sy 5)) (drew sx sy))
 ;(sleep 2)
 ;(random 5)
 ;list的set!-car在racket中用不了
@@ -12,21 +17,34 @@
                         (drew (+ n 1))
                         )))
 (define (setnum) (let ((ptr (random 9))) (if (= (vector-ref map ptr) 0) (vector-set! map ptr 2) (setnum))))
-(define (up n)(begin (set! n 0) (cond ((or (= n 0) (= n 1) (= n 2));变量n用于递归记次,调用时填0
-                                            (cond ((= (vector-ref map n) 0) (vector-set! map n (vector-ref map (+ n 3))))
-                                                  ((= (vector-ref map n) (vector-ref map (+ n 3))) (vector-set! map n (+ (vector-ref map n) (vector-ref map (+ n 3)))))))
+(define (U n) (begin (cond ((or (= n 0)(= n 1)(= n 2)) (when (= (vector-ref map n) 0) (begin (vector-set! map n (vector-ref map (+ n 3)))
+                                                                                                 (vector-set! map (+ n 3) 0)))
+                                                       (when (and (= (vector-ref map n) 0) (= (vector-ref map (+ n 3)) 0)) (begin (vector-set! map n (vector-ref map (+ n 6)))
+                                                                                                 (vector-set! map (+ n 6) 0))))
+                          ((or (= n 3)(= n 4)(= n 5)) (when (= (vector-ref map n) 0) (begin (vector-set! map n (vector-ref map (+ n 3)))
+                                                                                                 (vector-set! map (+ n 3) 0)))))
+                     (when (< n 5) (U (+ n 1)))))
+                           
+(define (up n)(begin (when (= n 0)(U n)) (cond ((or (= n 0) (= n 1) (= n 2));变量n用于递归记次,调用时填0
+                                            (cond ((= (vector-ref map n) (vector-ref map (+ n 3))) (begin (vector-set! map n (+ (vector-ref map n) (vector-ref map (+ n 3)))) 
+                                                                                                          (vector-set! map (+ n 3) (vector-ref map (+ n 6)))
+                                                                                                                   (vector-set! map (+ n 6) 0))))) 
+                                                  ;((= (vector-ref map n) (vector-ref map (+ n 3))) (begin (vector-set! map n (+ (vector-ref map n) (vector-ref map (+ n 3)))) 
+                                                                                                         ; (vector-set! map (+ n 3) (vector-ref map (+ n 6))
+                                                                                                                  ; (vector-set! map (+ n 6) 0)))))
                                        ((or (= n 3) (= n 4) (= n 5))
-                                            (cond ((= (vector-ref map n) 0) (vector-set! map n (vector-ref map (+ n 3))))
-                                                  ((= (vector-ref map n) (vector-ref map (+ n 3))) (vector-set! map n (+ (vector-ref map n) (vector-ref map (+ n 3))))))))
-                     (up (+ n 1))))
+                                            (cond ((= (vector-ref map n) (vector-ref map (+ n 3))) (begin (vector-set! map n (+ (vector-ref map n) (vector-ref map (+ n 3))))
+                                                                                                          (vector-set! map (+ n 3) 0))))))  (if (>= n 5) (newline) (up (+ n 1)))))
 
-(define (play) (λ (sg) (begin (set! sg (read))
-                              (cond ((= sg "w") (up)) ((= sg "s") (down)) ((= sg "a") (lift)) ((= sg "d") (right)))
+(define (play) (let ((sg (read))) (begin (cond ((string=? sg "w") (up 0)) ((string=? sg "s") (up 0)) ((string=? sg "a") (up 0)) ((string=? sg "d") (up 0)))
                               (setnum) (drew 0) (play))))  
 (define (game-begin) (begin
-(setnum)
-(setnum)
+        (display "2048 begin:")
+        (newline)(newline)
+        (setnum)(setnum)
 (drew 0)))
 
 (game-begin)
+(play)
+
 
